91f9a8442b8cacd8a20a4218af83d4be667dd94d
Gilson Santos <gilsonsantos1998@gmail.com>
2022-01-06 20:48:34 -0300

    Move FeedStore to its own file

---
+++ EssentialFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timeStamp: Date, completion: @escaping InsertionCompletion)
+}
