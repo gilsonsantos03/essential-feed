8ea6cb2d581be8bb6aaf030912b5df0d80ec33f1
Gilson Santos <gilsonsantos1998@gmail.com>
2022-01-13 11:49:09 -0300

    Dispatch CodableFeedStore operation in a serial background queue to avoid blocking clients and ADD noDeletePermissionURL method to fix error on test_delete_deliversErrorOnDeletionError test

--- EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -114,7 +114,7 @@ class CodableFeedStoreTests: XCTestCase {
     }
     
     func test_delete_deliversErrorOnDeletionError() {
-        let noDeletePermissionURL = cachesDirectory()
+        let noDeletePermissionURL = noDeletePermissionURL()
         let sut = makeSUT(storeURL: noDeletePermissionURL)
 
         let deletionError = deleteCache(from: sut)
@@ -226,5 +226,9 @@ class CodableFeedStoreTests: XCTestCase {
     
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+    
+    private func noDeletePermissionURL() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .systemDomainMask).first!
     }
 }
