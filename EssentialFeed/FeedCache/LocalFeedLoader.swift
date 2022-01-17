1838bca2fb0d9d5f67cdf1bdb3c0dc7ecd080544
Gilson Santos <gilsonsantos1998@gmail.com>
2022-01-17 12:21:45 -0300

    Refactor CachedFeed type from enum to tuple since we can represent of a value with an Optional

--- EssentialFeed/FeedCache/LocalFeedLoader.swift
+++ EssentialFeed/FeedCache/LocalFeedLoader.swift
@@ -44,8 +44,8 @@ extension LocalFeedLoader: FeedLoader {
             switch result {
             case let .failure(error):
                 completion(.failure(error))
-            case let .success(.found(feed, timestamp)) where FeedChacePolicy.validate(timestamp, against: self.currentDate()):
-                completion(.success(feed.toModels()))
+            case let .success(.some(cache)) where FeedChacePolicy.validate(cache.timestamp, against: self.currentDate()):
+                completion(.success(cache.feed.toModels()))
             case .success:
                 completion(.success([]))
             }
@@ -61,7 +61,7 @@ extension LocalFeedLoader {
             switch result {
             case .failure:
                 self.store.deleteCachedFeed { _ in }
-            case let .success(.found(_, timestamp)) where !FeedChacePolicy.validate(timestamp, against: self.currentDate()):
+            case let .success(.some(cache)) where !FeedChacePolicy.validate(cache.timestamp, against: self.currentDate()):
                 self.store.deleteCachedFeed { _ in }
             case .success:
                 break
