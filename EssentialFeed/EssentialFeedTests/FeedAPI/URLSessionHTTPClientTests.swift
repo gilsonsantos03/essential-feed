2e89dce980c096e5f4935c6d9c098d842fe6b3da
Gilson Santos <gilsonsantos1998@gmail.com>
2022-02-18 19:37:52 -0300

    RemoteFeedImageDataLoader.loadImageDataFromURL delivers non-empty received data on 200 HTTP response AND remove warnings

--- EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -86,7 +86,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         let result = resultFor(data: data, response: response, error: error, file: file, line: line)
        
         switch result {
-        case .success(let data, let response):
+        case let .success(( data, response)):
             return (data, response)
         default:
             XCTFail("Expected success, got \(result) instead", file: file, line: line)
