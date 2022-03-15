7b52ab99b3d64de6472eae08357b63a5ff661e2e
Gilson Santos <gilsonsantos1998@gmail.com>
2022-03-14 22:40:03 -0300

    Move FeedLoaderCacheDecorator to production

--- EssentialApp/EssentialAppTests/FeedLoadCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoadCacheDecoratorTests.swift
@@ -1,24 +1,6 @@
-import XCTest
+import EssentialApp
 import EssentialFeed
-
-final class FeedLoaderCacheDecorator: FeedLoader {
-    private let decoratee: FeedLoader
-    private let cache: FeedCache
-    
-    init(decoratee: FeedLoader, cache: FeedCache) {
-        self.decoratee = decoratee
-        self.cache = cache
-    }
-    
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        decoratee.load { [weak self] result in
-            completion(result.map { feed in
-                self?.cache.save(feed) { _ in }
-                return feed
-            })
-        }
-    }
-}
+import XCTest
 
 class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderTestCase {
     
