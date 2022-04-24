7250071f7ddd996560e7dd1a88283c471674099e
Gilson Santos <gilsonsantos1998@gmail.com>
2022-04-24 18:16:44 -0300

    Move "GENERIC_CONNECTION_ERROR" localization key to new Shared.strings

---
+++ EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+Localization.swift
@@ -0,0 +1,17 @@
+import Foundation
+import XCTest
+import EssentialFeed
+
+extension FeedUIIntegrationTests {
+    private class DummyView: ResourceView {
+        func display(_ viewModel: Any) {}
+    }
+    
+    var loadError: String {
+        LoadResourcePresenter<Any, DummyView>.loadError
+    }
+    
+    var feedTitle: String {
+        FeedPresenter.title
+    }
+}
