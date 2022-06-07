c14fff8236b92f785004873442d992a5e9067be5
Gilson Santos <gilsonsantos1998@gmail.com>
2022-06-07 18:28:26 -0300

    Prevent loading resources again until a running request completes

--- EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/CommentsUIIntegrationTests.swift
@@ -24,8 +24,13 @@ class CommentsUIIntegrationTests: XCTestCase {
         XCTAssertEqual(loader.loadCommentsCallCount, 1, "Expected a loading request once view is loaded")
         
         sut.simulateUserInitiatedReload()
+        XCTAssertEqual(loader.loadCommentsCallCount, 1, "Expected no request until previous completes")
+        
+        loader.completeCommentsLoading(at: 0)
+        sut.simulateUserInitiatedReload()
         XCTAssertEqual(loader.loadCommentsCallCount, 2, "Expected another loading request once user initiates a reload")
         
+        loader.completeCommentsLoading(at: 1)
         sut.simulateUserInitiatedReload()
         XCTAssertEqual(loader.loadCommentsCallCount, 3, "Expected yet another loading request once user initiates another reload")
     }
@@ -100,7 +105,7 @@ class CommentsUIIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
 
-    func test_loadFeedCompletion_rendersErrorMessageOnErrorUntilNextReload() {
+    func test_loadCommentsCompletion_rendersErrorMessageOnErrorUntilNextReload() {
         let (sut, loader) = makeSUT()
         
         sut.loadViewIfNeeded()
@@ -128,9 +133,9 @@ class CommentsUIIntegrationTests: XCTestCase {
     
     func test_deinit_cancelsRunningRequest() {
         var cancelCallCount = 0
-
+        
         var sut: ListViewController?
-
+        
         autoreleasepool {
              sut = CommentsUIComposer.commentsComposedWith(commentsLoader: {
                 PassthroughSubject<[ImageComment], Error>()
@@ -138,14 +143,14 @@ class CommentsUIIntegrationTests: XCTestCase {
                         cancelCallCount += 1
                     }).eraseToAnyPublisher()
             })
-
+            
             sut?.loadViewIfNeeded()
         }
-
+                
         XCTAssertEqual(cancelCallCount, 0)
-
+        
         sut = nil
-
+        
         XCTAssertEqual(cancelCallCount, 1)
     }
     
@@ -190,6 +195,7 @@ class CommentsUIIntegrationTests: XCTestCase {
 
         func completeCommentsLoading(with comments: [ImageComment] = [], at index: Int = 0) {
             requests[index].send(comments)
+            requests[index].send(completion: .finished)
         }
         
         func completeCommentsLoadingWithError(at index: Int = 0) {
